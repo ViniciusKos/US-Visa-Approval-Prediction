1c03b8312ca64a2f83d0e6cff6bcb1e758944a77
vinicius kos <kos.mota28@hotmail.com>
2024-06-05 08:28:06 -0300

    reorganize folders

--- demo.py
+++ demo.py
@@ -1,11 +1,14 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
+# from us_visa.logger import logging
+# from us_visa.exception import USvisaException
+# import sys
 
 
-try:
-    a = 1 / "10"
-    logging.info("deu bom")
-except Exception as e:
-    logging.info("deu ruim")
-    raise USvisaException(e, sys) from e
+# try:
+#     a = 1 / "10"
+#     logging.info("deu bom")
+# except Exception as e:
+#     logging.info("deu ruim")
+#     raise USvisaException(e, sys) from e
+
+from us_visa.constants import DATABASE_NAME
+print(DATABASE_NAME)
